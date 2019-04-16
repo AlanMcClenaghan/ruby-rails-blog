9b0660747dec061e60d3cd91cb6efcc849459294
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-04-16 14:28:38 +0100

    Routes to Update Actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,9 +9,22 @@ class PagesController < ApplicationController
     @page = Page.new
   end
   def create
-    page_params = params.require(:page).permit(:title, :body, :slug)
     @page = Page.new(page_params)
     @page.save
     redirect_to @page
   end
+  def edit
+    @page = Page.find(params[:id])
+  end
+  def update
+    @page = Page.find(params[:id])
+    @page.update(page_params)
+    redirect_to @page
+  end
+
+  private
+
+    def page_params
+      params.require(:page).permit(:title, :body, :slug)
+    end
 end
