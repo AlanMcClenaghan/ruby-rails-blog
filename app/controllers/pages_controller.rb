f7b1a16807f723a34b5e3b504688673bf916a971
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-04-16 14:46:35 +0100

    A Route to a Delete Action, Rails Resources and Filters.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,24 +1,36 @@
 class PagesController < ApplicationController
+
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+
   def index
     @pages = Page.all
   end
+
   def show
-    @page = Page.find(params[:id])
+
   end
+
   def new
     @page = Page.new
   end
+
   def create
     @page = Page.new(page_params)
     @page.save
     redirect_to @page
   end
+
   def edit
-    @page = Page.find(params[:id])
+
   end
+
   def update
-    @page = Page.find(params[:id])
     @page.update(page_params)
+    redirect_to @page
+  end
+
+  def destroy
+    @page.destroy
     redirect_to @page
   end
 
@@ -26,5 +38,9 @@ class PagesController < ApplicationController
 
     def page_params
       params.require(:page).permit(:title, :body, :slug)
+    end
+
+    def set_page
+      @page = Page.find(params[:id])
     end
 end
