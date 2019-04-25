b8d394c17387b9606e07868a49e473d4064ee4d3
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-04-25 15:47:06 +0100

    Active Record Associations and Using Associations in Your Apps

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
+
   resources :pages
 
   # Above is the shortcut for seven conventional routes below:
